f6f2231cabaeb40dab9a0a12a0aa86a60b35efc1
blakolukas <lucasblacovicz@gmail.com>
2025-06-16 19:48:11 -0300

    mudando group

--- src/main/java/com/projarq/nameServer/NameServerApplication.java
+++ src/main/java/com/projarq/nameServer/NameServerApplication.java
@@ -1,4 +1,4 @@
-package com.imposto.nameServer;
+package com.projarq.nameServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
