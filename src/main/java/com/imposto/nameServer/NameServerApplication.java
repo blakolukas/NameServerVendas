388c216287d08385c7128db214da1aa6c893e8de
blakolukas <lucasblacovicz@gmail.com>
2025-06-16 19:13:53 -0300

    primeiro commit

---
+++ src/main/java/com/imposto/nameServer/NameServerApplication.java
@@ -0,0 +1,13 @@
+package com.imposto.nameServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameServerApplication.class, args);
+	}
+
+}
